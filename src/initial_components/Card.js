41237ba8161d8161c8b8121c3261f884bbbd451c
mihlarwaseem <wazeemmuhammdh998@gmail.com>
2023-01-17 13:11:34 +0530

    style changes made

--- src/initial_components/Card.js
+++ src/initial_components/Card.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Card(props) {
   return (
-    <div className='project-card'>
-    <img src={props.image} alt="Image"/>
-    <h2 className='project-title'>{props.title}</h2>
-    <div className='project-details'>
-        <p>{props.text}</p> 
-        <div className='project-button'>
-        <a href={props.link} target='_blank' className={"btn"}>Source Code</a>             
+    <div className="project-card">
+      <img src={props.image} alt="Image" />
+      <h2 className="project-title">{props.title}</h2>
+      <div className="project-details">
+        <p>{props.text}</p>
+
+        <div className="project-button">
+          <a href={props.link} target="_blank" className={"btn"}>
+            Source Code
+          </a>
         </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- src/initial_components/Heroimg.js
+++ src/initial_components/Heroimg.js
@@ -1,39 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Introimg from '../accets/Intro.jpg'
-import '../index.css'
-import './HeroimgStyle.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import Introimg from "../accets/Intro.jpg";
+import "../index.css";
+import "./HeroimgStyle.css";
 
 function Heroimg() {
   return (
-    <div className='hero'>
-        <div className='mask'>
-        <img className='intro-img' src={Introimg}
-        alt={"Intro-Image"}/>
+    <div className="hero">
+      <div className="mask">
+        <img className="intro-img" src={Introimg} alt={"Intro-Image"} />
+      </div>
+      <div className="content">
+        <p>HI I'M WASEEM</p>
+        <h1>FULL STACK DEVELOPER</h1>
+        <div>
+          <Link to={"/project"}>
+            <button className="btn">Project</button>
+          </Link>
+          <Link to={"/contact"}>
+            <button className="btn btn-light">Contact</button>
+          </Link>
         </div>
-        <div className='content'>
-            <p>HI I'M WASEEM</p>
-            <h1>WEB & MOBILE DEVELOPER</h1>
-            <div>
-                <Link to={"/project"}>
-                    <button className='btn'>Project</button>
-                </Link>
-                <Link to={"/contact"}>
-                    <button className='btn btn-light'>Contact</button>
-                </Link>
-            </div>
-        </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default Heroimg
-
-
-
-
-
-
-
-
+export default Heroimg;
