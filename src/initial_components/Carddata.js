6e741f3c208af089351f48b7aa3e471a546ae7a6
mihlarwaseem <wazeemmuhammdh998@gmail.com>
2023-01-28 20:10:25 +0530

    Add a project

--- src/initial_components/Carddata.js
+++ src/initial_components/Carddata.js
@@ -1,37 +1,40 @@
-import image1 from '../accets/foodOrder.webp';
-import image2 from '../accets/secureimage2.png';
-import image3 from '../accets/All-app.jpg';
-import image4 from '../accets/Mern-memory.png';
-const Carddata=[
+import image1 from "../accets/foodOrder.webp";
+import image2 from "../accets/secureimage2.png";
+import image3 from "../accets/All-app.jpg";
+import image4 from "../accets/Mern-memory.png";
+import image5 from "../accets/openai.png";
 
-	{
-        image:image4,
-        title:"MERN CRUD Application",
-        text:"Full stack web Application Using Mongodb, Express-js, React-js, Node-js. with Authendication and Authorization process, Google login API, fully Responsive, MUI use for UI",
-        link:"https://github.com/waseemmihlar/MERN-memories-frontend.git"
-    },
-    {
-        image:image1,
-        title:"Online Food-Odering",
-        text:"Web application. Written in Bootstrap, MySQL, Php, JavaScript and Developed with team of 4 colleagues",
-        link:"https://github.com/waseemmihlar/food_order.git"
-    },
-    {
-        image:image2,
-        title:"Image secure Application",
-        text:"Mobile Application, using Java, Sqlite, Firebase cloud storage",
-        link:"https://github.com/waseemmihlar/image_store.git"
-    },
-    {
-        image:image3,
-        title:"Other Project Application",
-        text:"React Ui Pages, Other Mern Project,",
-        link:"https://github.com/waseemmihlar"
-    },
-	
+const Carddata = [
+  {
+    image: image4,
+    title: "MERN CRUD Application",
+    text: "Full stack web Application Using Mongodb, Express-js, React-js, Node-js. with Authendication and Authorization process, Google login API, fully Responsive, MUI use for UI",
+    link: "https://github.com/waseemmihlar/MERN-memories-frontend.git",
+  },
+  {
+    image: image5,
+    title: "MERN AI generate Image APP",
+    text: "Full stack web Application Using Mongodb, Express-js, React-js, Node-js. generate images from OpenAI API,Ui develped with tailwindCSS",
+    link: "https://github.com/waseemmihlar/Fullstack_OpenAI_Image_Loader.git",
+  },
+  {
+    image: image1,
+    title: "Online Food-Odering",
+    text: "Web application. Written in Bootstrap, MySQL, Php, JavaScript and Developed with team of 4 colleagues",
+    link: "https://github.com/waseemmihlar/food_order.git",
+  },
+  {
+    image: image2,
+    title: "Image secure Application",
+    text: "Mobile Application, using Java, Sqlite, Firebase cloud storage",
+    link: "https://github.com/waseemmihlar/image_store.git",
+  },
+  {
+    image: image3,
+    title: "Other Project Application",
+    text: "React Ui Pages, Other Mern Project,",
+    link: "https://github.com/waseemmihlar",
+  },
+];
 
-
-]
-
-export default Carddata
-
+export default Carddata;
